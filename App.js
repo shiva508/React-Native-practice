c53132645b1d208b83667af5bf30c3cf168754fe
shivad <shivad@prodigygroupindia.com>
2020-03-27 14:50:06 +0530

    REACT-NATIVE

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Checkbox } from 'react-native-paper';
+import EmojiDict from './EmojiDict'
+export default function App() {
+  
+  return (
+    <View style={styles.container}>
+      <Text>H! app</Text>
+      <Checkbox></Checkbox>
+      <EmojiDict></EmojiDict>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
